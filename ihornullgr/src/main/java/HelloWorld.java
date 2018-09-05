f86458751a9388c01cb6f29cb310bb505b8b3173
ihornullgr <i.levchenko@nullgr.com>
2018-09-05 20:53:53 +0300

    1.1

--- ihornullgr/src/main/java/HelloWorld.java
+++ ihornullgr/src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello Evgeniy!");
+        System.out.println("Hello Evgeniy!");
+        System.out.println("Hello Evgeniy!");
+        System.out.println("Hello Evgeniy!");
     }
 }
