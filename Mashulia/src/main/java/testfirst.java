f32425fcc723df82cd5dc2bcc02814c3d3690780
Mariya Golodnyuk <mashulia.g@gmail.com>
2018-09-08 01:02:26 +0300

    added Module Mashulia 1.0 added testfirst

---
+++ Mashulia/src/main/java/testfirst.java
@@ -0,0 +1,31 @@
+public class testfirst {
+    public static void main(String[] args) {
+        int a = 87;
+
+        int a1 = a/2;
+        int dec1 = a%2;
+        int a2 = a1/2;
+        int dec2 = a1%2;
+        int a3 = a2/2;
+        int dec3 = a2%2;
+        int a4 = a3/2;
+        int dec4 = a3%2;
+        int a5 = a4/2;
+        int dec5 = a4%2;
+        int a6 = a5/2;
+        int dec6 = a5%2;
+        int a7 = a6/2;
+        int dec7 = a6%2;
+
+        System.out.print(dec7);
+        System.out.print(dec6);
+        System.out.print(dec5);
+        System.out.print(dec4);
+        System.out.print(dec3);
+        System.out.print(dec2);
+        System.out.println(dec1);
+
+        // System.out.println(Integer.toBinaryString(a));
+
+    }
+}
