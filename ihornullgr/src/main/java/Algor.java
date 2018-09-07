fbaa755d1943b7b64634f68f605e903e94660c77
ihornullgr <i.levchenko@nullgr.com>
2018-09-08 01:24:32 +0300

    practica 1.3

--- ihornullgr/src/main/java/Algor.java
+++ ihornullgr/src/main/java/Algor.java
@@ -1,15 +1,28 @@
 public class Algor {
     public static void main(String[] args) {
-        int a = 64;
+        int a = 77;
 
-        int b = a % 2;
+        int a1 = a / 2;
+        int b1 = a % 2;
 
+        int a2 = a1 / 2;
+        int b2 = a1 % 2;
 
-        int c = a % 2;
-        int c1 = a % 2;
-        int c2 = 8 % 2;
-        int c3 = 4 % 2;
-        int c4 = 2 % 2;
-        System.out.print(a = b + c + c1 + c2 + c3 + c4);
+        int a3 = a2 / 2;
+        int b3 = a2 % 2;
+
+        int a4 = a3 / 2;
+        int b4 = a3 % 2;
+
+        int a5 = a4 / 2;
+        int b5 = a4 % 2;
+
+        int a6 = a5 / 2;
+        int b6 = a5 % 2;
+
+        int a7 = a6 / 2;
+        int b7 = a6 % 2;
+
+        System.out.println(a + " = " + b7 + b6 + b5 + b4 + b3 + b2 + b1);
     }
 }
