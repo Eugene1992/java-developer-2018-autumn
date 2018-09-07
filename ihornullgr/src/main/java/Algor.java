0f72c048ee8d3894390b657b37e9705a49fe2c56
ihornullgr <i.levchenko@nullgr.com>
2018-09-07 22:26:00 +0300

    Merge branch 'master' of /Users/i.levchenko/Desktop/java-developer-2018-autumn with conflicts.

---
+++ ihornullgr/src/main/java/Algor.java
@@ -0,0 +1,15 @@
+public class Algor {
+    public static void main(String[] args) {
+        int a = 64;
+
+        int b = a % 2;
+
+
+        int c = a % 2;
+        int c1 = a % 2;
+        int c2 = 8 % 2;
+        int c3 = 4 % 2;
+        int c4 = 2 % 2;
+        System.out.print(a = b + c + c1 + c2 + c3 + c4);
+    }
+}
--- src/main/java/java_starter/variables/Test.java
+++
@@ -1,9 +0,0 @@
-package java_starter.variables;
-
-public class Test {
-    public static void main(String[] args) {
-        int a = 52; // 1 ... 127
-
-
-    }
-}
