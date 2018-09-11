28cd770422bc4052a205a1434aa4b0ec3e172e34
ihornullgr <i.levchenko@nullgr.com>
2018-09-11 15:40:01 +0300

    interval done

--- ihornullgr/src/main/java/homeWork/HomeWork001.java
+++ ihornullgr/src/main/java/homeWork/HomeWork001.java
@@ -8,18 +8,31 @@ public class HomeWork001 {
         Создать программу, проверяющую и сообщающую на экран, является ли введенное с
         консоли целое число, чётным либо нечётным.*/
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число");
-        int nextInt = scan.nextInt();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Введите цифру или число");
+        int nextInt = scan2.nextInt();
 
         if (nextInt % 2 == 0) {
-            System.out.println("Четное");
+            System.out.println("Четное ");
         } else
-            System.out.println("Нечетное");
-        if (nextInt > 9) {
-            System.out.print("число");
+            System.out.println("Нечетное ");
+       /* if (nextInt > 9) {
+            System.out.print(" число");
         } else
-            System.out.print("цифра");
+            System.out.print(" цифра");*/
+
+        /*Задание 3
+        Создать программу, выводящую на экран ближайшее к 10 из двух чисел,
+        введенных с консоли.
+        Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.*/
+
+        Scanner scan3 = new Scanner(System.in);
+        System.out.println("Введите цифру или число 3");
+        int nextInt2 = scan3.nextInt();
+
+
     }
+
 }
+
 
---
+++ ihornullgr/src/main/java/homeWork/Interval.java
@@ -0,0 +1,22 @@
+package homeWork;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ввод");
+        int nextInt = scan.nextInt();
+
+        if (nextInt >= 0 && nextInt <= 14) {
+            System.out.println("0 - 14");
+        } else if (nextInt >= 15 && nextInt <= 35) {
+            System.out.println("15 - 35");
+        } else if (nextInt >= 36 && nextInt <= 50) {
+            System.out.println("36 - 50");
+        }else if (nextInt >= 51 && nextInt <= 100) {
+            System.out.println("51 - 100");
+        } else
+            System.out.print("Такого блока нет");
+    }
+}
