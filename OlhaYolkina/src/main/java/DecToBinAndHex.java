f62d55d694f74c560ea2c72043f3cbe608e112a7
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-12 21:13:05 +0300

    Dec To Bin and Hex

--- OlhaYolkina/src/main/java/DecToBinAndHex.java
+++ OlhaYolkina/src/main/java/DecToBinAndHex.java
@@ -20,5 +20,63 @@ public class DecToBinAndHex {
         while (dec != 0);
 
         System.out.println(number + " in Bin = " + binNumber);
+
+        int dec16, remainder16;
+        String hexNumber = "";
+        dec16 = number / 16;
+        remainder16 = number % 16;
+
+        if (remainder16 > 9) {
+            switch (remainder16) {
+                case 10:
+                    hexNumber = "A" + hexNumber;
+                    break;
+                case 11:
+                    hexNumber = "B" + hexNumber;
+                    break;
+                case 12:
+                    hexNumber = "C" + hexNumber;
+                    break;
+                case 13:
+                    hexNumber = "D" + hexNumber;
+                    break;
+                case 14:
+                    hexNumber = "E" + hexNumber;
+                    break;
+                case 15:
+                    hexNumber = "F" + hexNumber;
+                    break;
+            }
+        } else hexNumber = remainder16 + hexNumber;
+
+        do {
+            remainder16 = dec16 % 16;
+            dec16 = dec16 / 16;
+            if (remainder16 > 9) {
+                switch (remainder16) {
+                    case 10:
+                        hexNumber = "A" + hexNumber;
+                        break;
+                    case 11:
+                        hexNumber = "B" + hexNumber;
+                        break;
+                    case 12:
+                        hexNumber = "C" + hexNumber;
+                        break;
+                    case 13:
+                        hexNumber = "D" + hexNumber;
+                        break;
+                    case 14:
+                        hexNumber = "E" + hexNumber;
+                        break;
+                    case 15:
+                        hexNumber = "F" + hexNumber;
+                        break;
+                }
+            } else hexNumber = remainder16 + hexNumber;
+        }
+        while (dec16 != 0);
+
+        System.out.println(number + " in Hex = " + hexNumber);
     }
 }
