601aa01eab093e7020e1b253d854ab5035b8330f
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-16 19:56:49 +0300

    Sea Battle - second commit

--- OlhaYolkina/src/main/java/SeaBattle.java
+++ OlhaYolkina/src/main/java/SeaBattle.java
@@ -6,23 +6,29 @@ public class SeaBattle {
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
 
-        boolean[][] userMatrix = new boolean[4][4];
-        boolean[][] compMatrix = new boolean[4][4];
+        int tableHeight = 4;
+        int tableWidth = 4;
 
-        System.out.println("Please choose 4 cells for game in format [x, y]. Possible values 0 - 3");
+        int[][] userMatrix = new int[4][4];
+        int[][] compMatrix = new int[4][4];
+
+        System.out.println("Please choose 4 cells for game in format [x,y]. Possible values 0 - 3");
 
 
-        for (int i = 0; i < userMatrix.length; i++) {
-            String coordinate;
-            coordinate = scan.next();
-            int a = coordinate.toCharArray()[1] - '0';
-            userMatrix[coordinate.toCharArray()[1]][coordinate.toCharArray()[3]] = true;
+        for (int i = 0; i < tableHeight; i++) {
+                String coordinate;
+                coordinate = scan.next();
+                int x = coordinate.toCharArray()[1] - '0';
+                int y = coordinate.toCharArray()[3] - '0';
+
+                userMatrix[x][y] = 1;
         }
 
-        for (int i = 0; i < userMatrix.length; i++) {
-            for (int j = 0; j < userMatrix.length; j++) {
-                System.out.print("arr[" + i + "][" + j + "]= " + userMatrix[i][j]);
+        for (int i = 0; i < tableHeight; i++) {
+            for (int j = 0; j < tableWidth; j++) {
+                System.out.print(userMatrix[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
