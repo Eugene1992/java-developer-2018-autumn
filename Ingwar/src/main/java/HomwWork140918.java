21760ae49020920ff77c738a4077338c838b1080
Ingwar13 <ingwarflint@gmail.com>
2018-09-17 14:06:21 +0300

    Homework 140918 second part

--- Ingwar/src/main/java/HomwWork140918.java
+++ Ingwar/src/main/java/HomwWork140918.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class HomwWork140918 {
     public static void main(String[] args) {
         //1. Вывести в консоль все парные числа одномерного массива.
-        /*int[] intArray = {1,2,3,4,5,6,7,8,9,10};
+        int[] intArray = {1, 8, -3, 0, 3, -6, 5, 4, 3, 2};
         System.out.println("Initial array:");
-        System.out.println(Arrays.toString(intArray));*/
+        System.out.println(Arrays.toString(intArray));
 
         /*System.out.println("Even numbers are:");
         for (int count = 0; count < intArray.length; count++){
@@ -87,10 +87,7 @@ public class HomwWork140918 {
         System.out.println(Arrays.toString(intArrayNegative));*/
 
         //9. Подсчитать одинаковые числа в одномерном массиве.
-        /*int[] intArray = {1,2,3,4,3,6,5,8,3,2};
-        System.out.println("Initial array:");
-        System.out.println(Arrays.toString(intArray));
-        System.out.println("Equal numbers in array:");
+        /*System.out.println("Equal numbers in array:");
         int countEqual;
         int element;
         for (int count = 0; count < intArray.length; count++){
@@ -105,10 +102,7 @@ public class HomwWork140918 {
         }*/
 
         //10. Поменять минимальное и максимальное числа в массиве местами.
-        /*int[] intArray = {1,2,3,4,3,6,5,8,3,2};
-        System.out.println("Initial array:");
-        System.out.println(Arrays.toString(intArray));
-        int minNumber = intArray[0];
+        /*int minNumber = intArray[0];
         int minIndex = 0;
         int maxNumber = intArray[0];
         int maxIndex = 0;
@@ -129,10 +123,7 @@ public class HomwWork140918 {
         System.out.println(Arrays.toString(intArray));*/
 
         //11. Инвертировать массив.
-        /*int[] intArray = {1,2,3,4,3,6,5,8,3,2};
-        System.out.println("Initial array:");
-        System.out.println(Arrays.toString(intArray));
-        int[] intArrayTarget = new int[10];
+        /*int[] intArrayTarget = new int[10];
         for (int count = intArray.length-1; count >=0 ; count--) {
             intArrayTarget[intArray.length-1-count] = intArray[count];
         }
@@ -140,6 +131,127 @@ public class HomwWork140918 {
         System.out.println(Arrays.toString(intArrayTarget));*/
 
         //12. Вывести в консоль половину массива, среднеарифметическое которых является большим.
+        /*int halfArray = intArray.length/2;
+        int sumFirst = 0;
+        int sumSecond = 0;
+        for (int count = 0; count < halfArray; count++){
+            sumFirst = sumFirst+intArray[count];
+        }
+        for (int count = halfArray; count < intArray.length; count++){
+            sumSecond = sumSecond + intArray[count];
+        }
+        System.out.println("Greater half of array");
+        if (sumFirst/halfArray > sumSecond/(intArray.length-halfArray)) {
+            for (int count = 0; count < halfArray; count++) {
+                System.out.print(intArray[count] + " ");
+            }
+        }else
+            for (int count = halfArray; count < intArray.length; count++){
+                    System.out.print(intArray[count]+" ");
+            }*/
+
+        //13. Найти сумму отрицательных чисел в одномерном массиве.
+        /*int sumArrayNegative = 0;
+        for (int count = 0; count < intArray.length; count++){
+            if (intArray[count] < 0) {
+                sumArrayNegative = sumArrayNegative + intArray[count];
+            }
+        }
+        System.out.println("Sum of array is "+sumArrayNegative);*/
+
+        //14. Дан массив чисел, среди которых имеется один ноль. Вывести на печать все числа до нуля включительно.
+        /*for (int count = 0;count < intArray.length;count++){
+            if (intArray[count] == 0) {
+                break;
+            }else System.out.print(intArray[count]+ " ");
+        }*/
+
+        //15. Написать программу, меняющую местами два наибольших элемента одномерного массива с первым и последним.
+        /*int firstGreat = intArray[0];
+        int firstIndex = 0;
+        int secondGreat = intArray[0];
+        int secondIndex = 0;
+        int tempGreat = 0;
+        for (int count = 0;count < intArray.length;count++){
+            if (intArray[count] > firstGreat) {
+                tempGreat = firstGreat;
+                firstGreat = intArray[count];
+                if (tempGreat > secondGreat){
+                    secondGreat = tempGreat;
+                    secondIndex = firstIndex;
+                }
+                firstIndex = count;
+            } else if (intArray[count]>secondGreat){
+                secondGreat = intArray[count];
+                secondIndex = count;
+            }
+        }
+        intArray[secondIndex] = intArray[0];
+        intArray[0] = secondGreat;
+        intArray[firstIndex] = intArray[intArray.length-1];
+        intArray[intArray.length-1] = firstGreat;
+        System.out.println("Result array:");
+        System.out.println(Arrays.toString(intArray));*/
+
+        //16. Написать программу, которая выводит на экран элементы одномерного массива,
+        //которые расположены до минимального элемента всего массива.
+        /*int minNumber = intArray[0];
+        int minIndex = 0;
+        for (int count = 0;count < intArray.length;count++){
+            if (intArray[count] < minNumber) {
+                minNumber = intArray[count];
+                minIndex = count;
+            }
+        }
+        System.out.println("Array untill minimium:");
+        for (int count = 0;count<minIndex;count++){
+            System.out.print(intArray[count]+" ");
+        }*/
+
+        //17. Найти сумму всех чисел находящихся между минимальным и максимальным числом в массиве.
+        /*int minNumber = intArray[0];
+        int minIndex = 0;
+        int maxNumber = intArray[0];
+        int maxIndex = 0;
+        for (int count = 1; count < intArray.length; count++){
+            if (minNumber > intArray[count]) {
+                minNumber = intArray[count];
+                minIndex = count;
+            }else if (maxNumber < intArray[count]) {
+                maxNumber = intArray[count];
+                maxIndex = count;
+            }
+        }
+        if (maxIndex>minIndex) {
+            System.out.println("Numbers between minimum and maximun:");
+            for (int count = minIndex + 1; count < maxIndex; count++) {
+                System.out.print(intArray[count] + " ");
+            }
+        } else for (int count = maxIndex + 1; count < minIndex; count++) {
+            System.out.print(intArray[count] + " ");
+        }*/
+
+        //18. Вывести в консоль половину массива в которой находиться наименьшее число массива.
+        /*int minNumber = intArray[0];
+        int minIndex = 0;
+        for (int count = 0;count < intArray.length;count++){
+            if (intArray[count] < minNumber) {
+                minNumber = intArray[count];
+                minIndex = count;
+            }
+        }
+        System.out.println("The half of array with minimum:");
+        int halfArray = intArray.length/2;
+        if (minIndex < halfArray){
+            for (int count = 0;count<halfArray;count++){
+                System.out.print(intArray[count]);
+            }
+        } else for (int count = halfArray;count<intArray.length;count++){
+            System.out.print(intArray[count]+ " ");
+        }*/
+
+        //19. Написать программу, которая перемещает в конец массива все элементы,
+        // значения которых находится в отрезке [a,b].
 
     }
 }
