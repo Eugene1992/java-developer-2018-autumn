d03056c19e283d6a9faf967fd089fdf953d7f1dd
Mariya Golodnyuk <mashulia.g@gmail.com>
2018-09-21 18:06:52 +0300

    added Home Work 01

---
+++ Mashulia/src/main/java/HomeWork02/HomeWork_01.java
@@ -0,0 +1,10 @@
+package HomeWork02;
+
+public class HomeWork_01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//1. Создайте программу, выводящую на экран
+//все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 ….
