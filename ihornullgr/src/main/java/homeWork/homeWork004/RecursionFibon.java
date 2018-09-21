0ec989f541af43e1d2d3f3b71c1e227c9a0e0d3f
ihornullgr <i.levchenko@nullgr.com>
2018-09-21 18:56:11 +0300

    Fibonacci 1.1

--- ihornullgr/src/main/java/homeWork/homeWork004/RecursionFibon.java
+++ ihornullgr/src/main/java/homeWork/homeWork004/RecursionFibon.java
@@ -14,7 +14,9 @@ public class RecursionFibon {
     }
 
     static int fibonacci(int n) {
-        if ((n == 1) || (n == 2)) return 1;
+        if ((n == 1) || (n == 2)) {
+            return 1;
+        }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
---
+++ ihornullgr/src/main/java/homeWork/homeWork004/Refacror003.java
@@ -0,0 +1,51 @@
+package homeWork.homeWork004;
+
+public class Refacror003 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 22, 10, 55, 54};
+        System.out.println("Максимальное " + getMaxArrayEl(arr));
+        System.out.println("Минимальное " + getMinArrayEl(arr));
+        System.out.println("Парные " + prepaidNumbers(arr));//????
+        System.out.println("Первая половина массива " + firstMass(arr));
+
+    }
+
+    static String prepaidNumbers(int[] arr) {
+        String str = "";
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 == 0) {
+               str += Integer.toString(arr[i]) + ", ";
+            }
+        }
+        return str;
+    }
+
+    static int getMaxArrayEl(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    static int getMinArrayEl(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    static int firstMass (int[] arr){
+        int firstM = arr[0];
+        for (int i = 0; i < arr.length / 2; i++) {
+            firstM = arr[i];
+        }
+        return firstM;
+    }
+}
+
