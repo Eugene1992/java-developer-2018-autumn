382906435adfbbe8a70071b31a0edd9bd9f49718
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-22 00:27:38 +0300

    MaxSubString fix

--- OlhaYolkina/src/main/java/MaxSubString.java
+++ OlhaYolkina/src/main/java/MaxSubString.java
@@ -5,30 +5,37 @@ public class MaxSubString {
         System.out.println("First string = " + str1);
         System.out.println("Second string = " + str2);
 
-        String maxSubString = maxSubString(str1, str2);
+        String maxSubString = getMaxSubstring(str1, str2);
         System.out.println("Max substring:");
 
         if (maxSubString.equals("")) {
             System.out.println("Equals substrings are not found.");
         } else {
-            System.out.println(maxSubString(str1, str2));
+            System.out.println(getMaxSubstring(str1, str2));
         }
     }
 
-    static String maxSubString(String str1, String str2) {
+    static String getMaxSubstring(String str1, String str2) {
         String strLong = str1.length() > str2.length() ? str1 : str2;
         String strShort = str1.length() <= str2.length() ? str1 : str2;
 
         char[] charsLong = strLong.toCharArray();
         char[] charsShort = strShort.toCharArray();
 
+        int[] maxPositions = getMaxSubstringPositions(charsLong, charsShort);
+        int indexFrom = maxPositions[0];
+        int indexTo = maxPositions[0] + maxPositions[1];
+        return strLong.substring(indexFrom, indexTo);
+    }
+
+    static int[] getMaxSubstringPositions(char[] charsLong, char[] charsShort) {
         int indexFrom = 0;
         int max = 0;
 
         for (int i = 0; i < charsLong.length; i++) {
             for (int j = 0; j < charsShort.length; j++) {
                 if (charsLong[i] == charsShort[j]) {
-                    int count = quantityEqualsChars(charsLong, charsShort, i, j, 0);
+                    int count = getQuantityOfEqualChars(charsLong, charsShort, i, j, 0);
                     if (count > max) {
                         max = count;
                         indexFrom = i;
@@ -36,14 +43,14 @@ public class MaxSubString {
                 }
             }
         }
-        return strLong.substring(indexFrom, indexFrom + max);
+        return new int[]{indexFrom, max};
     }
 
-    static int quantityEqualsChars(char[] chars1, char[] chars2, int posFirstStr, int posSecondStr, int max) {
-        if ((posFirstStr == chars1.length) || (posSecondStr == chars2.length)
-                || (chars1[posFirstStr] != chars2[posSecondStr]))
+    static int getQuantityOfEqualChars(char[] chars1, char[] chars2, int posFirstStr, int posSecondStr, int max) {
+        if (posFirstStr == chars1.length || posSecondStr == chars2.length
+                || chars1[posFirstStr] != chars2[posSecondStr])
             return max;
         else
-            return quantityEqualsChars(chars1, chars2, posFirstStr + 1, posSecondStr + 1, max + 1);
+            return getQuantityOfEqualChars(chars1, chars2, posFirstStr + 1, posSecondStr + 1, max + 1);
     }
 }
