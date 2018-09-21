b09ec02618a57525c4a45f02729af7576d5672e8
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-21 23:39:26 +0300

    MaxSubString fix

--- OlhaYolkina/src/main/java/MaxSubString.java
+++ OlhaYolkina/src/main/java/MaxSubString.java
@@ -2,8 +2,8 @@ import static java.lang.Math.min;
 
 public class MaxSubString {
     public static void main(String[] args) {
-        String str1 = "Hello world name!";
-        String str2 = "Zeellion name";
+        String str1 = "hello world";
+        String str2 = "worship hell";
         System.out.println("First string = " + str1);
         System.out.println("Second string = " + str2);
 
@@ -31,7 +31,7 @@ public class MaxSubString {
 
         for (int i = 0; i < charsLong.length; i++) {
             int j = 0;
-            while (!(j == charsShort.length - 1)) {
+            while (j != charsShort.length - 1) {
                 if (charsLong[i] == charsShort[j]) {
                     count = quantityEqualsChars(charsLong, charsShort, i, j, count);
                     if (count > max) {
