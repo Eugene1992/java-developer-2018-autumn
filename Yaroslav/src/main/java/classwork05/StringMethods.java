61be189a06ead1d504b1ea47a68c3be95d259afd
yaserzhan <ya.serzhan@gmail.com>
2018-09-22 01:59:12 +0300

    add maxSimpleSubstring method

--- Yaroslav/src/main/java/classwork05/StringMethods.java
+++ Yaroslav/src/main/java/classwork05/StringMethods.java
@@ -15,10 +15,41 @@ public class StringMethods {
     }
 
 
-    public static void main(String[] args) {
-        String str1 = "Hello World!";
-        System.out.println(subString(str1, 1, 6));
+    private static String maxSimpleSubstring(String str1, String str2) {
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        String[] result = new String[100];
+        int wordCounter = 0;
+        for (int i = 0; i < chars1.length; i++) {
+            for (int j = 0; j < chars2.length; j++) {
+                if (chars1[i] == chars2[j]) {
+                    result[wordCounter] = "" + chars1[i];
+                    for (int k = 1; k < chars1.length - i; k++) {
+                        if (chars1[i + k] == chars2[j + k]) {
+                            result[wordCounter] += chars1[i + k];
+                        } else {
+                            wordCounter++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        String maxResult = "";
+        for (int i = 0; i < wordCounter; i++) {
+            if (maxResult.length() < result[i].length()) {
+                maxResult = result[i];
+            }
+        }
+        return maxResult;
+    }
 
-        //String str2 = "Hello, People!";
+
+    public static void main(String[] args) {
+        String str1 = "Hello,__WWWWorld!";
+        //System.out.println(subString(str1, 1, 6));
+        String str2 = "__WWWWorld,Hello,People!";
+
+        System.out.println(maxSimpleSubstring(str1, str2));
     }
 }
