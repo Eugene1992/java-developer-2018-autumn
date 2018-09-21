3ff2599b18bd212f303a8f600c73091361381a89
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-21 20:52:07 +0300

    class String - next commit

--- OlhaYolkina/src/main/java/classString.java
+++ OlhaYolkina/src/main/java/classString.java
@@ -4,21 +4,33 @@ public class classString {
     public static void main(String[] args) {
         String str = new String("Hi! My name is Olga!");
         Random random = new Random();
-        int firstPosition = random.nextInt(str.length());
-        int secondPosition = random.nextInt(str.length());
+        int indexFrom = random.nextInt(str.length());
+        int indexTo = random.nextInt(str.length());
 
-        System.out.println("First position - " + firstPosition + " Second position - " + secondPosition);
-        System.out.println(subString(str, firstPosition, secondPosition));
+        System.out.println("First position - " + indexFrom + " Second position - " + indexTo);
+        System.out.println(subString(str, indexFrom, indexTo));
     }
 
-    static String subString(String str, int firstPosition, int secondPosition) {
+    static String subString(String str, int indexFrom, int indexTo) {
         String newStr = "";
         char[] chars = str.toCharArray();
 
-        for (int i = firstPosition; i <= secondPosition; i++){
-            newStr += chars[i];
+        if (indexTo >= indexFrom) {
+            for (int i = indexFrom; i <= indexTo; i++) {
+                newStr += chars[i];
+            }
+        } else {
+            System.out.println("indexTo < indexFrom");
         }
 
         return newStr;
+    }
+
+    static String subString(String str, int indexFrom) {
+        return subString(str, indexFrom, str.length() - 1);
+    }
+
+    static String subString(int indexTo, String str) {
+        return subString(str, 0, indexTo);
     }
 }
