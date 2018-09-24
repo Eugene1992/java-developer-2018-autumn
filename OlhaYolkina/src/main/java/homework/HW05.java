0156489495b90480102d06f5468cafdb4489292d
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-24 15:47:34 +0300

    HW05

--- OlhaYolkina/src/main/java/homework/HW05.java
+++ OlhaYolkina/src/main/java/homework/HW05.java
@@ -3,11 +3,17 @@ package homework;
 public class HW05 {
     public static void main(String[] args) {
         String str = "   Hello   world   ";
+        String substring = "world";
+        int[] arr = new int[]{1, 3, 4, 5};
+
         System.out.println("Trim : " + trim(str));
         System.out.println("Absolute trim : " + trimAll(str));
         System.out.println("Index of 'l' = " + indexOf(str, 'l'));
         System.out.println("Index of 'a' = " + indexOf(str, 'a'));
         System.out.println("Last index of 'l' = " + lastIndexOf(str, 'l'));
+        System.out.println("Contains : " + contains(str, substring));
+        System.out.println("To String : " + toString(arr));
+        System.out.println("Replace all : " + replaceAll(str, substring, "hi"));
     }
 
     /**
@@ -79,5 +85,86 @@ public class HW05 {
         }
 
         return -1;
+    }
+
+    /**
+     * - contains (от)
+     */
+    static boolean containsFrom(String str, String substring, int indexFrom) {
+        char[] strChars = str.toCharArray();
+        char[] substringChars = substring.toCharArray();
+
+        if (strChars.length < substringChars.length) {
+            return false;
+        } else {
+            for (; indexFrom < strChars.length; indexFrom++) {
+                if (strChars[indexFrom] == substringChars[0]) {
+                    int k = indexFrom;
+                    int j = 0;
+
+                    while ((k < strChars.length) && (j < substringChars.length) && (strChars[k] == substringChars[j])) {
+                        k++;
+                        j++;
+                    }
+
+                    if (j == substringChars.length) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * - contains
+     */
+    static boolean contains(String str, String substring) {
+        return containsFrom(str, substring, 0);
+    }
+
+    /**
+     * - toString(int[] arr) new int[]{1, 3, 4, 5} -> "[1, 3, 4, 5]"
+     */
+    static String toString(int[] arr) {
+        String str = "[";
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            str += arr[i] + ", ";
+        }
+
+        return str + arr[arr.length - 1] + "]";
+    }
+
+    /**
+     * - replaceAll
+     */
+    static String replaceAll(String str, String strReplace, String strInsert) {
+        if (!contains(str, strReplace)) {
+            return str;
+        } else {
+            char[] strChars = str.toCharArray();
+            char[] strReplaceChars = strReplace.toCharArray();
+            String newStr = "";
+
+            for (int i = 0; i < strChars.length; i++) {
+                if (strChars[i] == strReplaceChars[0]) {
+                    int k = i;
+                    int j = 0;
+                    while ((k < strChars.length) && (j < strReplaceChars.length) && (strChars[k] == strReplaceChars[j])) {
+                        k++;
+                        j++;
+                    }
+
+                    if (j == strReplaceChars.length) {
+                        newStr += strInsert;
+                        i += j;
+                    }
+                } else {
+                    newStr += strChars[i];
+                }
+            }
+            return newStr;
+        }
     }
 }
