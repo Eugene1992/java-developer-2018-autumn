25bc0ee1150f700ec7e85cc44c54442b78fb880f
ihornullgr <i.levchenko@nullgr.com>
2018-09-24 12:57:39 +0300

    substr 1.1

--- ihornullgr/src/main/java/workPractice/SubString.java
+++ ihornullgr/src/main/java/workPractice/SubString.java
@@ -2,11 +2,27 @@ package workPractice;
 
 public class SubString {
     public static void main(String[] args) {
-        String str1 = "Hello world!";
-        String str2 = "worship hell!";
-        System.out.println(substring(str1, 0, 4));
+        System.out.println("От и до");
+        String str1 = "   Hello       world!  ";
+        System.out.println(substring(str1, 3, 7));
         System.out.println("");
+
+        System.out.println("Максимальное предложение");
+        String str2 = "   worship hell!";
         System.out.println(maxSubstring(str1, str2));
+        System.out.println("");
+
+        System.out.println("Обрезан по бокам");
+        System.out.println(trim(str1));
+        System.out.println("");
+
+
+
+        System.out.println("toString");
+        int[] array = new int[]{1, 3, 4, 5};
+        System.out.println(toString(array));
+        System.out.println("");
+
 
     }
 
@@ -32,11 +48,41 @@ public class SubString {
             }
         }
         for (int i = 0; i < chars2.length; i++) {
-            if (chars2.length > chars1.length){
+            if (chars2.length > chars1.length) {
                 result += chars2[i];
             }
         }
         return result;
+    }
+
+    static String trim(String strTrim) {
+        String trimString = "";
+        char[] chars = strTrim.toCharArray();
+        int firstIndex = 0;
+        int secondIndex = 0;
+        for (int i = 0; chars[i] == ' '; i++) {
+            firstIndex = i + 1;
+        }
+        for (int i = chars.length - 1; chars[i] == ' '; i--) {
+            secondIndex = i - 1;
+        }
+        trimString = substring(strTrim, firstIndex, secondIndex + 1);
+        return trimString;
+    }
+
+    static String substring(String str, int to) {
+        return substring(str, 0, to);
+    }
+
+    //- toString(int[] arr) new int[]{1, 3, 4, 5} -> "[1, 3, 4, 5]"
+    static String toString(int[] array) {
+        String str = "";
+        for (int i = 0; i < array.length; i++) {
+            str += (array[i] + ", ");
+
+        }
+        str = "[" + substring(str, str.length() - 2)  + "]";
+        return str;
     }
 }
 
