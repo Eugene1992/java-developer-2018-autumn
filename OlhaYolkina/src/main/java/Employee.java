86b0852b1b2134189b0739c89236d2c608d8c8ac
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-09-24 20:25:46 +0300

    max salary fix

--- OlhaYolkina/src/main/java/Employee.java
+++ OlhaYolkina/src/main/java/Employee.java
@@ -1,13 +1,12 @@
 public class Employee {
     public static void main(String[] args) {
         String[][] employee = new String[][]{{"Henry", "25", "1500"}, {"Julia", "34", "1000"}, {"Maks", "27", "2000"}};
-        int size = 3;
-        System.out.println(maxSalary(employee, size)[0]);
+        System.out.println(maxSalary(employee)[0]);
     }
 
-    static String[] maxSalary(String[][] employee, int size) {
+    static String[] maxSalary(String[][] employee) {
         int max = 0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < employee.length; i++) {
             if (Integer.parseInt(employee[i][2]) > max) {
                 max = i;
             }
