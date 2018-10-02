de3643f47d2cf343baa9c77749acb28d438c694f
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-10-03 00:28:15 +0300

    game RPG game process merge

--- OlhaYolkina/src/main/java/javaEssential/homework/gameRPG/GameProcess.java
+++ OlhaYolkina/src/main/java/javaEssential/homework/gameRPG/GameProcess.java
@@ -1,12 +1,9 @@
 package javaEssential.homework.gameRPG;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class GameProcess {
-    private static final Scanner scan = new Scanner(System.in);
     private static final Random random = new Random();
-    private static int teamSize = 0;
 
     public void startGame() {
         System.out.println("Hi! It is RPG game Fight.");
@@ -16,80 +13,26 @@ public class GameProcess {
         System.out.println("Go!!!");
     }
 
-    public static int getTeamSize() {
-        return teamSize;
+    public Hero[] stepFight(Hero[] teamAttack, Hero[] teamArmor) {
+
+        int indexHeroAttack = random.nextInt(teamAttack.length);
+        int indexHeroArmor = random.nextInt(teamArmor.length);
+
+        int heroArmorHealth = teamArmor[indexHeroArmor].getHealth();
+        double heroArmorArmor = teamArmor[indexHeroArmor].getArmor();
+
+        int heroAttackAttack = teamAttack[indexHeroAttack].getAttack();
+
+        teamArmor[indexHeroArmor].setHealth((int) (heroArmorHealth - (1 - heroArmorArmor) * heroAttackAttack));
+
+
+
+        logStepFight(teamAttack[indexHeroAttack], teamArmor[indexHeroArmor]);
+
+        return teamArmor;
     }
 
-    public static void setTeamSize() {
-        while (teamSize <= 0) {
-            System.out.println("Please tap size of Team. Put number > 0");
-            teamSize = scan.nextInt();
-        }
-    }
-
-    public Player isItComputer(Player player) {
-        String answer = "";
-        while (!answer.equals("Yes") && !answer.equals("No")) {
-            System.out.println("Is this user is Computer? Please write Yes or No");
-            answer = scan.next();
-            player.setIsComputer(answer);
-        }
-        return player;
-    }
-
-    public Hero[] makeTeam(Player player) {
-        Hero[] team = new Hero[teamSize];
-        for (Hero aHero : team) {
-            if (player.getIsComputer()) {
-                aHero = chooseRaceByRandom();
-            } else {
-                aHero = chooseRaceByScreen();
-            }
-        }
-        return team;
-    }
-
-    private Hero chooseRaceByScreen() {
-        String race = "";
-        Hero aHero;
-        while (!race.equals("O") && !race.equals("E") && !race.equals("H") && !race.equals("D")) {
-            System.out.println("Please choose a race of Hero: O - Ork, E - Elf, H - Human and D - Dwarf");
-            race = scan.next();
-        }
-        switch (race) {
-            case "O":
-                aHero = new Ork("ork");
-                return aHero;
-            case "E":
-                aHero = new Elf("elf");
-                return aHero;
-            case "H":
-                aHero = new Human("human");
-                return aHero;
-            case "D":
-                aHero = new Dwarf("dwarf");
-                return aHero;
-        }
-        return null;
-    }
-
-    private Hero chooseRaceByRandom() {
-        int num = random.nextInt(4);
-        Hero aHero;
-        switch (num) {
-            case 0:
-                aHero = new Ork("ork");
-                return aHero;
-            case 1:
-                aHero = new Elf("elf");
-                return aHero;
-            case 2:
-                aHero = new Human("human");
-                return aHero;
-            case 3:
-                aHero = new Dwarf("dwarf");
-                return aHero;
-        }
-        return null;
+    private void logStepFight(Hero heroAttack, Hero heroArmor) {
+        System.out.println("Player Dwarf 'Astan'[340hp] attacked PC Elf 'Eliot'[321hp], damage caused is 121 hp");
     }
 }
--- OlhaYolkina/src/main/java/javaEssential/homework/gameRPG/Player.java
+++ OlhaYolkina/src/main/java/javaEssential/homework/gameRPG/Player.java
@@ -1,8 +1,15 @@
 package javaEssential.homework.gameRPG;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Player {
-    private String user = "user";
+    private static final Scanner scan = new Scanner(System.in);
+    private static final Random random = new Random();
+
+    private String user = "User";
     private boolean isComputer;
+    private Hero[] team;
 
     public String getUser() {
         return this.user;
@@ -12,10 +19,77 @@ public class Player {
         return isComputer;
     }
 
-    public void setIsComputer(String answer) {
+    public void setComputer() {
+        String answer = "";
+        while (!answer.equals("Yes") && !answer.equals("No")) {
+            System.out.println("Is this user is Computer? Please write Yes or No");
+            answer = scan.next();
+        }
         if (answer.equals("Yes")) {
             this.isComputer = true;
-            this.user = "computer";
+            this.user = "Computer";
         }
+    }
+
+    public void setTeam() {
+        int teamSize = 0;
+
+        while (teamSize <= 0) {
+            System.out.println("Please tap size of Team. Put number > 0");
+            teamSize = scan.nextInt();
+        }
+
+        this.team = new Hero[teamSize];
+        for (Hero aHero : team) {
+            if (this.getIsComputer()) {
+                aHero = chooseRaceByRandom();
+            } else {
+                aHero = chooseRaceByScreen();
+            }
+        }
+    }
+
+    private Hero chooseRaceByScreen() {
+        String race = "";
+        Hero aHero;
+        while (!race.equals("O") && !race.equals("E") && !race.equals("H") && !race.equals("D")) {
+            System.out.println("Please choose a race of Hero: O - Ork, E - Elf, H - Human and D - Dwarf");
+            race = scan.next();
+        }
+        switch (race) {
+            case "O":
+                aHero = new Ork("ork");
+                return aHero;
+            case "E":
+                aHero = new Elf("elf");
+                return aHero;
+            case "H":
+                aHero = new Human("human");
+                return aHero;
+            case "D":
+                aHero = new Dwarf("dwarf");
+                return aHero;
+        }
+        return null;
+    }
+
+    private Hero chooseRaceByRandom() {
+        int num = random.nextInt(4);
+        Hero aHero;
+        switch (num) {
+            case 0:
+                aHero = new Ork("ork");
+                return aHero;
+            case 1:
+                aHero = new Elf("elf");
+                return aHero;
+            case 2:
+                aHero = new Human("human");
+                return aHero;
+            case 3:
+                aHero = new Dwarf("dwarf");
+                return aHero;
+        }
+        return null;
     }
 }
