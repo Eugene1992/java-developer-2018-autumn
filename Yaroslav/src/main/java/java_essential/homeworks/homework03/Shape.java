9aeb48155291bce0ba599668932cdc0e1c8163ff
yaserzhan <ya.serzhan@gmail.com>
2018-10-02 22:55:52 +0300

    add JavaEssential.homework03(Shape homework)

---
+++ Yaroslav/src/main/java/java_essential/homeworks/homework03/Shape.java
@@ -0,0 +1,13 @@
+package java_essential.homeworks.homework03;
+
+public abstract class Shape {
+    protected double a;
+
+    protected Shape(double a) {
+        this.a = a;
+    }
+
+    protected abstract double getArea();
+
+    public abstract String toString();
+}
