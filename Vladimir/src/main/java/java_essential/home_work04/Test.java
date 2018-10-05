283709d2419b63badb5fa39faf25d6dbe11e1b44
Volodymyr1404 <krasulyavova@ukr.net>
2018-10-05 13:17:03 +0300

    Commit for essential home04

---
+++ Vladimir/src/main/java/java_essential/home_work04/Test.java
@@ -0,0 +1,11 @@
+package java_essential.home_work04;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Lists objectForOutPutList = new Lists();
+        objectForOutPutList.outPutNewLinkedList();
+        objectForOutPutList.outPutNewArrayList();
+    }
+}
