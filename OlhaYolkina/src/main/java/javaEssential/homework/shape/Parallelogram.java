91f4420f8e73056f21ab241701e885373714ae9c
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-10-05 17:28:20 +0300

    Shape. Calculating square fix

--- OlhaYolkina/src/main/java/javaEssential/homework/shape/Parallelogram.java
+++ OlhaYolkina/src/main/java/javaEssential/homework/shape/Parallelogram.java
@@ -3,7 +3,7 @@ package javaEssential.homework.shape;
 import static java.lang.StrictMath.sin;
 
 public class Parallelogram extends Quadrilateral {
-    protected int alpha = (int) Math.toRadians(90);
+    protected int alpha = 90;
     protected int c = 4;
 
     public Parallelogram() {
@@ -16,6 +16,6 @@ public class Parallelogram extends Quadrilateral {
     }
 
     protected double calculateSquareThroughAngle() {
-        return a * c * sin(alpha);
+        return a * c * sin(Math.toRadians(alpha));
     }
 }
