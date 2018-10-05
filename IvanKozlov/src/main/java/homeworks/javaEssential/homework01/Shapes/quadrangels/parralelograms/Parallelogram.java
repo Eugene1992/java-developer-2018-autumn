a470d40aef443a33ca9b1484be02c4c72f32c4b0
Ivan Kozlov <ivan.kozlov@netent.com>
2018-10-05 19:06:15 +0300

    added shapes homework01

---
+++ IvanKozlov/src/main/java/homeworks/javaEssential/homework01/Shapes/quadrangels/parralelograms/Parallelogram.java
@@ -0,0 +1,9 @@
+package homeworks.javaEssential.homework01.Shapes.quadrangels.parralelograms;
+
+import homeworks.javaEssential.homework01.Shapes.Shape;
+
+public class Parallelogram extends Shape{
+    public Parallelogram(double sideLength, double heightLength) {
+        super(sideLength, heightLength);
+    }
+}
