8ed802377f4d36e6931e2a3f028d9decea985330
Olha.Yolkina <Olha.Yolkina@clicktale.com>
2018-10-05 15:30:49 +0300

    Shape. Calculating square fix

--- OlhaYolkina/src/main/java/javaEssential/homework/shape/Rhombus.java
+++ OlhaYolkina/src/main/java/javaEssential/homework/shape/Rhombus.java
@@ -1,7 +1,5 @@
 package javaEssential.homework.shape;
 
-import static java.lang.StrictMath.sin;
-
 public class Rhombus extends Parallelogram {
     public Rhombus() {
         this.name = "Rhombus";
