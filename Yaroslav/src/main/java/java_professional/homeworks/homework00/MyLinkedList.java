8c7c499edc299036bb2551781502011f4f347f6a
yaserzhan <ya.serzhan@gmail.com>
2018-10-19 04:49:35 +0300

    add MyLinkedList Homework

--- Yaroslav/src/main/java/java_professional/homeworks/homework00/MyLinkedList.java
+++ Yaroslav/src/main/java/java_professional/homeworks/homework00/MyLinkedList.java
@@ -53,6 +53,7 @@ public class MyLinkedList<E> implements List<E> {
 
     private class LinkedListIterator implements Iterator<E> {
         private Node<E> current = first;
+        private Node<E> currentInverse = last;
 
         public E next() {
             if (!hasNext()) { throw new NoSuchElementException(); }
@@ -64,9 +65,18 @@ public class MyLinkedList<E> implements List<E> {
         public boolean hasNext() { return current != null; }
     }
 
+    /**
+     * Метод, который возвращает масив созданый из списка
+     */
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] array = new Object[size];
+        int counter = 0;
+        for(E elem : this){
+            array[counter] = elem;
+            counter++;
+        }
+        return array;
     }
 
     @Override
@@ -79,24 +89,7 @@ public class MyLinkedList<E> implements List<E> {
      */
     @Override
     public boolean add(E elem) {
-        if(elem == null){
-            return false;
-        }
-        if(isEmpty()){
-            last = new Node<>(null, elem, null);
-            first = last;
-        }
-        else {
-            Node current = first;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current = last;
-            last = new Node<>(current, elem, null);
-            current.next = last;
-
-        }
-        size++;
+        add(size, elem);
         return true;
     }
 
@@ -115,49 +108,77 @@ public class MyLinkedList<E> implements List<E> {
             last = null;
             return true;
         }
-
-        Node current = first;
-        boolean isDeleted = false;
-        while(current.next != null){
-            if(isDeleted){
-                current.elem = current.next.elem;
-                current = current.next;
-            }
-            else if(current.elem.equals(elem)){
-                isDeleted = true;
-            }
-            else{
-                current = current.next;
-            }
-        }
-        if(isDeleted) {
-            last = current.prev;
-            last.next = null;
-            size--;
-            return true;
-        }
-        return false;
+        int index = indexOf(elem);
+        remove(index);
+        return true;
     }
 
 
+    /**
+     * Метод, который проверяет входят ли все элементы передвной
+     * колекции в список
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        if(c == null){
+            return false;
+        }
+        for(Object object : c){
+            if(!contains(object)){
+                return false;
+            }
+        }
+        return true;
     }
 
+
+    /**
+     * Метод, который добавляет в конец списка все элементы переданой
+     * колекции
+     */
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        if (c == null){
+            return false;
+        }
+        for(E elem : c){
+            add(elem);
+        }
+        return true;
     }
 
+
+    /**
+     * Метод, который вставляет в список все элементы переданой
+     * колекции. Вставляет начиная с номера єлемента index
+     */
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (c == null){
+            return false;
+        }
+        for(E elem : c){
+            add(index, elem);
+        }
+        return true;
     }
 
+
+    /**
+     * Метод, который удаляет из списка все элементы, значения которых равны любому
+     * из элементов переданой колекции
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        if (c == null){
+            return false;
+        }
+        for(Object object : c){
+            while(contains(object)){
+                remove(object);
+            }
+        }
+        return true;
     }
 
     @Override
@@ -165,43 +186,161 @@ public class MyLinkedList<E> implements List<E> {
         return false;
     }
 
+
+    /**
+     * Метод, который удаляет все элементы из списка
+     */
     @Override
     public void clear() {
-
+        first = null;
+        last = null;
+        size = 0;
     }
 
+    /**
+     * Метод, который возвращает элемент списка по индексу
+     */
     @Override
     public E get(int index) {
+        int counter = 0;
+        for(E elem : this){
+            if(counter == index){
+                return elem;
+            }
+            counter++;
+        }
         return null;
     }
 
+
+    /**
+     * Метод, который заменяет элемент списка по индексу на новый
+     */
     @Override
-    public E set(int index, E element) {
+    public E set(int index, E element){
+        Node current = first;
+        int counter = 0;
+        while(current != null) {
+            if (counter == index){
+                current.elem = element;
+                return element;
+            }
+            current = current.next;
+            counter++;
+        }
         return null;
     }
 
+
+    /**
+     * Метод, который добавляет обьект в список по заданому индексу
+     */
     @Override
     public void add(int index, E element) {
-        int counter = 0;
-        Node current = first;
-        while(counter != index ){
-            current = current.next;
+        if(isEmpty()){
+            last = new Node<>(null, element, null);
+            first = last;
+            size++;
+        }
+        else if(index == 0){
+            Node newNode = new Node(null, element, first);
+            first = newNode;
+            size++;
+        }
+        else if(index == size ){
+            Node newNode = new Node(last, element, null);
+            last.next = newNode;
+            last = newNode;
+            size++;
+        }
+        else if(index < size && index > 0 && element != null) {
+            int counter = 0;
+            Node current = first;
+            Node prevCurrent = first.prev;
+            while (counter != index) {
+                current = current.next;
+                prevCurrent = current.prev;
+                counter++;
+            }
+            Node newNode = new Node(prevCurrent, element, current);
+            current.prev = newNode;
+            prevCurrent.next = newNode;
+            size++;
         }
     }
 
+
+    /**
+     * Метод, который удаляет элемент списка по индексу
+     */
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index > size + 1 || isEmpty()){
+            return null;
+        }
+        Node<E> current;
+        if(index == 0){
+            current = first.next;
+            first = current;
+            first.prev = null;
+        }
+        else if(index == size - 1){
+            current = last.prev;
+            last = current;
+            last.next = null;
+        }
+        else{
+            current = first;
+            int counter = 0;
+            while (counter != index) {
+                current = current.next;
+                counter++;
+            }
+            Node<E> newNext = current.next;
+            Node<E> newPrev = current.prev;
+            newNext.prev = newPrev;
+            newPrev.next = newNext;
+        }
+        size--;
+        return current.elem;
     }
 
+
+    /**
+     * Метод, который находит индекс первого вхождения данного элемента
+     * в список
+     */
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int counter = 0;
+        Node current = first;
+        while(current != null){
+            if(current.elem.equals(o)){
+                return counter;
+            }
+            current = current.next;
+            counter++;
+        }
+        return -1;
     }
 
+
+    /**
+     * Метод, который находит индекс последнего вхождения данного
+     * элемента в список
+     */
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int counter = size - 1;
+        Node current = last;
+        while(current != null){
+            if(current.elem.equals(o)){
+                return counter;
+            }
+            current = current.prev;
+            counter--;
+        }
+        return -1;
     }
 
     @Override
@@ -219,34 +358,38 @@ public class MyLinkedList<E> implements List<E> {
         return null;
     }
 
+
+    /**Метод для вывода списка*/
+    @Override
+    public String toString() {
+        String res = "[";
+        int counter = 0;
+        Node current = first;
+        while(current != null) {
+            counter++;
+            if(counter == size){
+                res += current.elem;
+            }
+            else {
+                res += current.elem + ", ";
+            }
+            current = current.next;
+        }
+        res += "]";
+        return res;
+    }
+
+
+    /** Вспомагательный вложеный клас*/
     private static class Node<E> {
-        E elem;
-        Node<E> next;
-        Node<E> prev;
+        E elem;         /** значение самого элемента*/
+        Node<E> next;   /** отсылка на следующий элемент*/
+        Node<E> prev;   /** отсылка на предыдущий элемент*/
 
         Node(Node<E> prev, E element, Node<E> next) {
             this.elem = element;
             this.next = next;
             this.prev = prev;
         }
-    }
-
-
-
-    @Override
-    public String toString() {
-        String res = "[";
-        int counter = 0;
-        for (E elem : this) {
-            counter++;
-            if(counter == size){
-                res += elem;
-            }
-            else {
-                res += elem + ", ";
-            }
-        }
-        res += "]";
-        return res;
     }
 }
--- Yaroslav/src/main/java/java_professional/homeworks/homework00/Tester.java
+++ Yaroslav/src/main/java/java_professional/homeworks/homework00/Tester.java
@@ -1,26 +1,199 @@
 package java_professional.homeworks.homework00;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
         List<Integer> myLinkedList = new MyLinkedList<>();
-        System.out.println("Размер списка - " + myLinkedList.size());
-        System.out.println(myLinkedList);
-        myLinkedList.add(100);
-        myLinkedList.add(200);
-        myLinkedList.add(300);
-        myLinkedList.add(400);
-        myLinkedList.add(500);
-        System.out.println("Размер списка - " + myLinkedList.size());
-        System.out.println(myLinkedList);
-        myLinkedList.remove((Integer)300);
-        System.out.println("Размер списка - " + myLinkedList.size());
-        System.out.println(myLinkedList);
 
-        System.out.println(myLinkedList.contains(100));
-        System.out.println(myLinkedList.contains(400));
-        System.out.println(myLinkedList.contains(1000));
+
+
+        /** Тест методов isEmpty() и size()*/
+        System.out.println("\t\t\tTEST: isEmpty(), size()");
+        System.out.println("Список пустой? - " + myLinkedList.isEmpty());
+        System.out.println("Размер списка - " + myLinkedList.size());
+        System.out.println("Список" + myLinkedList);
+        System.out.println("\n");
+
+
+        /** Тест метода add(Object o)*/
+        System.out.println("\t\t\tTEST: add(Object o)");
+        int n = 100;
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Добавим элемент " + n);
+            myLinkedList.add(n);
+            n += 100;
+        }
+        System.out.println("Список пустой? - " + myLinkedList.isEmpty());
+        System.out.println("Размер списка - " + myLinkedList.size());
+        System.out.println("Список" + myLinkedList);
+        System.out.println("\n");
+
+
+
+        /** Тест метода contains(Object o)*/
+        System.out.println("\t\t\tTEST: contains(Object o)");
+        int a = 100;
+        int b = 3000;
+        System.out.println("Список" + myLinkedList);
+        System.out.println("Элемент " + a + " входит в список? - " + myLinkedList.contains(a));
+        System.out.println("Элемент " + b + " входит в список? - " + myLinkedList.contains(b));
+        System.out.println("\n");
+
+
+
+        /** Тест метода add(int index, Object element)*/
+        System.out.println("\t\t\tTEST: add(int index, Object element)");
+        int k = 100;
+        int index = 6;
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("Добавим элемент " + k + " по индексу " + index);
+        myLinkedList.add(index, k);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+        /** Тест методов remove(Object o) и remove(int index)*/
+        System.out.println("\t\t\tTEST: remove(Object o) и remove(int index)");
+        k = 400;
+        index = 3;
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("Удалим элемент " + k);
+        myLinkedList.remove((Object) k);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("Удалим элемент по индексу " + index);
+        myLinkedList.remove(index);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+        /** Тест метода get(int index)*/
+        System.out.println("\t\t\tTEST: get(int index)");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("Элемент по индексу " + i + " - " + myLinkedList.get(i));
+        }
+        System.out.println("\n");
+
+
+
+        /** Тест метода set(int index)*/
+        System.out.println("\t\t\tTEST: set(int index)");
+        k = 1000;
+        index = 6;
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Меняем элемент по индексу " + index + " на " + k);
+        myLinkedList.set(index, k);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+        /** Тест метода indexOf() и lastIndexOf()*/
+        System.out.println("\t\t\tTEST: indexOf() и lastIndexOf()");
+        k = 100;
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Индекс первого вхождения елемента " + k + " = " + myLinkedList.indexOf(k));
+        System.out.println("Индекс последнего вхождения елемента " + k + " = " + myLinkedList.lastIndexOf(k));
+        System.out.println("\n");
+
+
+
+        /** Тест метода addAll(int index, Collection c)*/
+        System.out.println("\t\t\tTEST: addAll(int index, Collection c)");
+        Collection linkedList = new LinkedList();
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        linkedList.add(77);
+        linkedList.add(88);
+        linkedList.add(99);
+        index = 2;
+        System.out.println("Добавляемая колекция - " + linkedList + " по индексу " + index);
+        myLinkedList.addAll(index, linkedList);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+        /** Тест метода addAll(Collection c)*/
+        System.out.println("\t\t\tTEST: addAll(Collection c)");
+        Collection arrayList = new ArrayList();
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        System.out.println("Добавляемая колекция - " + arrayList);
+        myLinkedList.addAll(arrayList);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+
+        /** Тест метода containsAll(Collection c)*/
+        System.out.println("\t\t\tTEST: containsAll(Collection c)");
+        arrayList = new ArrayList();
+        System.out.println("Список - " + myLinkedList);
+        arrayList.add(-12);
+        arrayList.add(-13);
+        arrayList.add(-14);
+        System.out.println("Колекция - " + arrayList);
+        System.out.println("Входит колекция в список? - " + myLinkedList.containsAll(arrayList));
+        arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(100);
+        System.out.println("Колекция - " + arrayList);
+        System.out.println("Входит колекция в список? - " + myLinkedList.containsAll(arrayList));
+        System.out.println("\n");
+
+
+
+        /** Тест метод removeAll(Collection c)*/
+        System.out.println("\t\t\tTEST: removeAll(Collection c)");
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        arrayList = new ArrayList();
+        arrayList.add(100);
+        arrayList.add(1);
+        System.out.println("Колекция(для удаления) - " + arrayList);
+        myLinkedList.removeAll(arrayList);
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("\n");
+
+
+
+        /** Тест метода clear()*/
+        System.out.println("\t\t\tTEST: clear()");
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("Удаляем все элементы методом clear()");
+        myLinkedList.clear();
+        System.out.println("Список - " + myLinkedList);
+        System.out.println("Размер списка = " + myLinkedList.size());
+        System.out.println("Пустой ли список? " + myLinkedList.isEmpty());
+
+/*
+
+
+
+        myLinkedList.clear();
+        System.out.println("Размер списка - " + myLinkedList.size());
+        System.out.println("Список" + myLinkedList);*/
     }
 }
