fcec400a90857b75f96a4c55eafed83535d32347
Ingwar13 <ingwarflint@gmail.com>
2018-10-26 15:52:25 +0300

    New linked list #5

--- Ingwar/src/main/java/java_professional/newLinkedList/NewUserLinkedList.java
+++ Ingwar/src/main/java/java_professional/newLinkedList/NewUserLinkedList.java
@@ -1,12 +1,13 @@
 package java_professional.newLinkedList;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
-public class NewUserLinkedList implements List {
+public class NewUserLinkedList<E> implements List<E> {
 
     private int size;
     private final ListNode FIRST_NODE = new ListNode();
@@ -14,6 +15,8 @@ public class NewUserLinkedList implements List {
 
     public NewUserLinkedList() {
         this.size = 0;
+		this.FIRST_NODE.nextNode = this.LAST_NODE;
+		this.LAST_NODE.previousNode = this.FIRST_NODE;
     }
 
     private class ListNode<E> {
@@ -55,34 +58,69 @@ public class NewUserLinkedList implements List {
         }
     }
 
-	@Override
-	public void add(int arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
+	public int getSize() {
+		return size;
+	}
+
+	private ListNode findNextNode(int index) {
+		if (index > this.size / 2) {
+			ListNode nextNode = this.LAST_NODE;
+			for (int i = size; i >= index; i--) {
+				nextNode = nextNode.previousNode;
+			}
+			return nextNode;
+		} else {
+			ListNode nextNode = this.FIRST_NODE;
+			for (int i = 1; i <= index; i++) {
+				nextNode = nextNode.nextNode;
+			}
+			return nextNode;
+		}
 	}
 
 	@Override
-	public boolean add(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public void add(int index, E element) {
+		ListNode nextNode = findNextNode(index);
+		ListNode newNode = new ListNode<>(nextNode.previousNode, nextNode, element);
+		nextNode.previousNode.nextNode = newNode;
+		nextNode.previousNode = newNode;
+		this.size++;
 	}
 
 	@Override
-	public boolean addAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean add(E element) {
+		this.add(size + 1, element);
+		return true;
 	}
 
 	@Override
-	public boolean addAll(int arg0, Collection arg1) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean addAll(Collection elements) {
+		this.addAll(this.size + 1, elements);
+		return true;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection elements) {
+		Object[] collection = elements.toArray();
+		for (int i = 0; i < collection.length; i++) {
+			this.add(index + i, (E) collection[i]);
+		}
+		return true;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		ListNode checkNode = this.LAST_NODE.previousNode;
+		while (checkNode.previousNode != null) {
+			ListNode deleteNode = checkNode;
+			checkNode = deleteNode.previousNode;
+			deleteNode.previousNode = null;
+			deleteNode.nextNode = null;
+			deleteNode.content = null;
+		}
+		this.size = 0;
+		this.LAST_NODE.previousNode = this.FIRST_NODE;
+		this.FIRST_NODE.nextNode = this.LAST_NODE;
 	}
 
 	@Override
@@ -98,7 +136,7 @@ public class NewUserLinkedList implements List {
 	}
 
 	@Override
-	public Object get(int arg0) {
+	public E get(int arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -140,7 +178,7 @@ public class NewUserLinkedList implements List {
 	}
 
 	@Override
-	public Object remove(int arg0) {
+	public E remove(int arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
